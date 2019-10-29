a6238a5e3e339b9877716fb3799fbe1aa7ef9262
Ayush Shrivastava <ayush16024@iiitd.ac.in>
2019-10-29 23:05:52 +0530

    Changed incoming call from hardcoded to dynamic

---
+++ app/src/main/java/com/example/mcfinalproject/IncomingCall.java
@@ -0,0 +1,15 @@
+package com.example.mcfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncomingCall extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_call);
+    }
+}
